cec97cfd04dd31e7cb5f02fc3b1b9f72b898a727
OhSuYeong <101083171+OhSuYeong@users.noreply.github.com>
2022-06-21 23:29:30 +0900

    Update main.py

--- E-on assignment8/main.py
+++ E-on assignment8/main.py
@@ -1,6 +1,7 @@
 import DataBase
 data = DataBase.TrainList #TrainList를 data에 저장
 ticket=[]
+k=0
 
 def menu() : #메뉴 출력 함수
     print("==기차표 예매 프로그램==")
@@ -12,27 +13,22 @@ def menu() : #메뉴 출력 함수
 def fast() : #빠른 시간 기차 검색 및 예매
     print("예매할 기차의 정보를 입력해주세요")
     new = input("\n시, 분, 출발역, 도착역, 열차종류(,로 구분) : ").split(",")    #new에 예약정보 저장
-    comp=[]
     for i in range(0,len(data)) : #리스트에서 리스트 길이만큼 반복해서
-             if(new[2] == data[i][2] and new[3] == data[i][3] and new[4] == data[i][4]) : #해당되는 리스트에서 출발역, 도착역, 열차종류가 같은 경우
-                comp=abs((new[0]*60+new[1])-(data[i][0]*60+data[i][1])) #그 중에 제일 가까운 시간을 찾기 위한 계산
-    for i in range(0,len(data)) : #리스트에서 리스트 길이만큼 반복해서
-            if(min(comp) == abs((new[0]*60+new[1])-(data[i][0]*60+data[i][1]))) :
-                print(data[i][0],":",data[i][1], "출발역 :", data[i][2], "도착역 :", data[i][3], "열차종류 :",data[i][4], "잔여석 :",data[i][5])
-                getin = int(input("\n예매하시겠습니까?(예매:1, 취소:2) "))
-                if getin==1 :
-                    data[i][5]= data[i][5]-1
-                    ticket = data[i][0]
-                    ticket = data[i][1]
-                    ticket = data[i][2]
-                    ticket = data[i][3]
-                    ticket = data[i][4]
-                elif getin==2 :
-                    close()
-                else:
-                    print("잘못된 접근입니다.")
+             if(new[2] == data[i][2] and new[3] == data[i][3] and new[4] == data[i][4] and new[0]*60+new[1] <= data[i][0]*60+data[i][1]) : #해당되는 리스트에서 출발역, 도착역, 열차종류가 같고 지나간 열차를 제외하고
+                k=i
+             else :
+                return 0
+    print("시간 :", data[k][0],":", data[k][1], "/ 출발 :", data[k][2], "/ 도착 :", data[k][3], "/ 열차종류 :", data[k][4], "/잔여좌석수 :", data[k][5])
+    get=input(int("해당 열차를 예매하시겠습니까?(네:1, 아니오:2)"))
+            if get==1 :
+                    ticket=data
+                    data[k][5]-=1
+            elif get==2 :
+                fast()
+            else :
+                print("잘못된 접근입니다.")
 
-def list_print() : #전체 기차 리스트 출력
+def alllist() : #전체 기차 리스트 출력
     for i in range(0,len(data)) : #리스트 길이만큼 반복
         print(i+1,"번", "시간 :", data[i][0],":", data[i][1], "/ 출발 :", data[i][2], "/ 도착 :", data[i][3], "/ 열차종류 :", data[i][4], "/잔여좌석수 :", data[i][5])
 
@@ -40,33 +36,16 @@ def modify() : #나의 예매 현황 출력 및 예매 취소
     print("==나의 예매 현황==")
     print("시간 :", ticket[0],":", ticket[1], "/ 출발 :", ticket[2], "/ 도착 :", ticket[3], "/ 열차종류 :", ticket[4])
     getin = int(input("\n예매를 취소하시겠습니까?(예:1, 아니오:2) "))
-        if getin==1 :
-            ticket = []
-        elif getin==2 :
-            close()
-        else:
+        if  getin == 1 :
+            ticket=[]
+            data[k][5]+=1
+        elif getin == 2 :
+            fast()
+        else :
             print("잘못된 접근입니다.")
 
-def save() : #저장 함수
-    file=open('C:\Users\오수영\python', "w") #쓰기 형태로 파일 열기
-    for i in range(0,len(data)) : #이중 리스트를 다시 txt파일 형태로 바꾸기
-        file.write(data[i][0])
-        file.write(" ")
-        file.write(data[i][1])
-        file.write(" ")
-        file.write(data[i][2])
-        file.write(" ")
-        file.write(data[i][3])
-        file.write(" ")
-        file.write(data[i][4])
-        file.write(" ")
-        file.write(data[i][5])
-        file.write("\n")
-    print("저장완료")
-    file.close()
-
-def close() : #프로그램 나가기(자동저장) 함수
-    exit(save())
+def close() : #프로그램 나가기 함수
+    exit()
 
 while True :
     menu()
@@ -75,7 +54,7 @@ while True :
     if num == 1 :
         fast()
     elif num == 2 :
-        list_print()
+        alllist()
     elif num == 3 :
         modify()
     elif num == 4 :
