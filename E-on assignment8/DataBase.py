2a0d287e51226ac51d8b8eac3c99169ca73c1ef6
OhSuYeong <101083171+OhSuYeong@users.noreply.github.com>
2022-06-21 23:28:58 +0900

    Update DataBase.py

--- E-on assignment8/DataBase.py
+++ E-on assignment8/DataBase.py
@@ -1,8 +1,10 @@
-file = open('C:\Users\오수영\python', "r")
+file = open('C:/Users/오수영/python/TrainList.txt', "r",encoding='UTF-8')
 TrainList = file.read().splitlines() #txt파일 내용을 \n 제거 후 TrainList에 저장
-TrainList = TrainList.replace(':', ' ')
-TrainList = TrainList.replace('-', '')
-TrainList = TrainList.replace('> ', '')
+del TrainList[0]
+for i in range(0,len(TrainList)) :
+    TrainList[i] = TrainList[i].replace(':', ' ')
+    TrainList[i] = TrainList[i].replace('-', '')
+    TrainList[i] = TrainList[i].replace('> ', '')
 for l in range(0,len(TrainList)) :      #TrainList를 이중 리스트로 구현
     n = TrainList[l].split()
     TrainList[l] = n
